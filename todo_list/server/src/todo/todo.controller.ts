ee3fb6f5a02c9a0c02362cee21ae73977c4dbd3f
dane <dhkdane@gmail.com>
2024-04-25 21:48:43 -0600

    dto

--- todo_list/server/src/todo/todo.controller.ts
+++ todo_list/server/src/todo/todo.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDto) {
+    console.log(createTodoDto);
+  }
+}
